5733f786eb274c42fad3cb44aa57438fa89d246a
Taruna Rawat <86361519+tarunarawat2218@users.noreply.github.com>
2023-07-21 18:19:27 +0530

    add database

--- backend/database.js
+++ backend/database.js
@@ -1,27 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
 
-const dbURI = process.env.DB_URI;
 
-// Connect to the MongoDB database
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// // Event listeners for database connection events
+// db.on('error', (error) => {
+//   console.error('MongoDB connection error:', error);
+// });
 
-// Get the default connection
-const db = mongoose.connection;
+// db.once('open', () => {
+//   console.log('Connected to MongoDB database');
+// });
 
-// Event listeners for database connection events
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-
-db.once('open', () => {
-  console.log('Connected to MongoDB database');
-});
-
-// Export the database connection
-module.exports = db;
+// // Export the database connection
+// module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
-const app = require("./app")
-require('dotenv').config();
+const app = require("./app");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+
+const dbURI = process.env.DB_URI;
+console.log('DB_URI:', dbURI);
+
+// Connect to the MongoDB database
+mongoose.connect( dbURI||"");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
