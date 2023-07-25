ed1f6b010deeee6ff5160ef086d94deebbe30ed5
Ashish <tarunarawat2218@gmail.com>
2023-07-25 19:35:13 +0530

    updated folder structure

--- backend/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addToCart, removeFromCart} = require('../controllers/cartController');
-
-
-const router =  express.Router()
-
-router.route('/cart').post(addToCart);
-router.route('/cart').post(removeFromCart);
-module.exports = router;
