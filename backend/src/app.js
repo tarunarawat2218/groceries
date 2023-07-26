cd9721ea8bf910ee68fc9b730833c9dacb5158ca
Ashish <tarunarawat2218@gmail.com>
2023-07-26 09:39:33 +0530

    added swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const app = express();
 app.use(express.json());
 
@@ -9,5 +10,8 @@ const cart = require('./routes/cartRoute')
 app.use('/api/v1', products);
 app.use('/api/v1/auth', users);
 app.use('/api/v1', cart);
+
+const swaggerDocument = YAML.load('./swagger.yaml'); // Replace with your Swagger specification file
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app; 
