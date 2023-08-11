4674a6c20117f1c848b6fb378e9cddf2e15ad5c5
Taruna Rawat <tarunarawat2218@gmail.com>
2023-08-11 12:40:10 +0530

    refactor

--- frontend/src/components/molecules/ListOfProduct.js
+++ frontend/src/components/molecules/ListOfProduct.js
@@ -25,19 +25,27 @@ export default function ListOfProduct() {
 
     return (
         <div>
-            <Grid container spacing={1}>
+            <Grid container spacing={3}>
                 {items.map(product => (
-                    <Grid item xs={12} md={3} key={product._id}>
-                        <Card sx={{ maxWidth: 345, marginBottom: "1rem", marginLeft: "2rem", borderRadius: 5 }}>
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
+                        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', borderRadius: 5 }}>
                             <CardMedia
                                 component="img"
                                 height="194"
                                 image={product.imageUrl}
                                 alt="Product Image"
                             />
-                            <Typography style={{ fontSize: "1.5rem", margin: "0.5rem", fontFamily: "rank Ruhl Libre, serif" }}>{product.name}</Typography>
-                            <Typography style={{ margin: "0.5rem", fontSize: "1rem", fontFamily: "rank Ruhl Libre, serif" }}>₹{product.price}</Typography>
-                            <Typography style={{ margin: "0.5rem", fontSize: "1rem", fontFamily: "rank Ruhl Libre, serif" }}>{product.description}</Typography>
+                            <div style={{ padding: '1rem' }}>
+                                <Typography style={{ fontSize: '1.5rem', fontFamily: 'rank Ruhl Libre, serif' }}>
+                                    {product.name}
+                                </Typography>
+                                <Typography style={{ fontSize: '1rem', fontFamily: 'rank Ruhl Libre, serif', color: 'gray' }}>
+                                    ₹{product.price}
+                                </Typography>
+                                <Typography style={{ fontSize: '1rem', fontFamily: 'rank Ruhl Libre, serif' }}>
+                                    {product.description}
+                                </Typography>
+                            </div>
                             <AddToCartButton productId={product._id} />
                         </Card>
                     </Grid>
